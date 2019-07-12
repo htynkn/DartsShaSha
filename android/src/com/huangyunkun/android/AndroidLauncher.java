7fef27736d621e18df616723c80e2ce95280e6b9
Huang Yunkun <htynkn@gmail.com>
2019-07-12 15:53:43 +0800

    修改依赖地址

--- android/src/com/huangyunkun/android/AndroidLauncher.java
+++ android/src/com/huangyunkun/android/AndroidLauncher.java
@@ -1,16 +1,19 @@
 package com.huangyunkun.android;
 
 import android.os.Bundle;
-
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.huangyunkun.DartsGame;
+import com.huangyunkun.android.extension.AndroidStatisticsService;
 
 public class AndroidLauncher extends AndroidApplication {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new DartsGame(), config);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        DartsGame.setStatisticsService(new AndroidStatisticsService());
+
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        initialize(new DartsGame(), config);
+    }
 }
---
+++ android/src/com/huangyunkun/android/extension/AndroidStatisticsService.java
@@ -0,0 +1,25 @@
+package com.huangyunkun.android.extension;
+
+import com.huangyunkun.extension.IStatisticsService;
+
+public class AndroidStatisticsService implements IStatisticsService {
+    @Override
+    public void onResume() {
+        
+    }
+
+    @Override
+    public void onPause() {
+
+    }
+
+    @Override
+    public void onEvent(String eventId, String label) {
+
+    }
+
+    @Override
+    public void onEvent(String eventId, String label, int count) {
+
+    }
+}
